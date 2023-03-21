c327a36484e2ba7837c727ccd2ba13c93001c76f
brian-nunez <bnunez9999@gmail.com>
2023-03-20 18:14:47 -0700

    feat(navigation): basic pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { useAutoAnimate } from '@formkit/auto-animate/react';
 
 import { trpc } from "../utils/trpc";
 
@@ -10,10 +11,14 @@ const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+  const [animateRef] = useAutoAnimate<HTMLDivElement>();
+
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <div ref={animateRef}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </div>
   );
 };
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        heroImage: "url('/images/bg-hero.jpg')",
+      },
+    },
   },
   plugins: [],
 };
